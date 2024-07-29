477253f8e42e041c7bf77ffa443b699fe6c92e61
jae9380 <ljy5314@gmail.com>
2024-07-29 13:29:15 +0900

    Section 2 / part 3 / Zuul-Filter

--- zuul-service/src/main/java/com/example/zuulservice/filter/ZuulLoggingFilter.java
+++ zuul-service/src/main/java/com/example/zuulservice/filter/ZuulLoggingFilter.java
@@ -10,6 +10,31 @@ import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @Component
-public class ZuulLoggingFilter  {
+public class ZuulLoggingFilter extends ZuulFilter  {
+
+    @Override
+    public Object run() throws ZuulException {
+        log.info("************* printing logs: " );
+
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        log.info("************* "+request.getRequestURI());
+
+        return null;
+    }
+    @Override
+    public String filterType() {
+        return "pre"; // 사전 필터이기 때문에 "pre"로 설정
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
 
 }
